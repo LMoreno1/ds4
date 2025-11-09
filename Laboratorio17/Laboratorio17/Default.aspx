689faad8f60138d7670d08432445342cefd356e5
LUIS MORENO <luis.moreno16@utp.ac.pa>
2025-11-09 18:22:50 -0500

    Laboratorio 17 completo

---
+++ Laboratorio17/Laboratorio17/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Laboratorio17._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <asp:GridView ID="GridView1" runat="server" 
+            DataSourceID="MyDataSource1"
+            AutoGenerateColumns="False"
+            DataKeyNames="ProductId"
+            AutoGenerateEditButton="True">
+            <Columns>
+                <asp:BoundField DataField="ProductId" HeaderText="ID" ReadOnly="True" />
+                <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+                <asp:BoundField DataField="UnitPrice" HeaderText="Unit Price" DataFormatString="{0:F2}" />
+            </Columns>
+        </asp:GridView>
+
+        <asp:SqlDataSource ID="MyDataSource1" runat="server"
+            ConnectionString="Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True"
+            ProviderName="System.Data.SqlClient"
+            SelectCommand="SELECT ProductId, ProductName, UnitPrice FROM Products"
+            UpdateCommand="UPDATE Products SET ProductName=@ProductName, UnitPrice=@UnitPrice WHERE ProductId=@ProductId">
+        </asp:SqlDataSource>
+    </div>
+</asp:Content>
---
+++ Laboratorio17/Laboratorio17/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Laboratorio17
+{
+    public partial class WebForm1 : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarDatosProcedimiento();
+            }
+        }
+
+        private void CargarDatosProcedimiento()
+        {
+            ConnectionStringSettings conString = ConfigurationManager.ConnectionStrings["ConexionNorthwind"];
+
+            using (SqlConnection conexion = new SqlConnection(conString.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("SalesByCategory", conexion))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@CategoryName", SqlDbType.VarChar).Value = "Seafood";
+
+                    conexion.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        GridV.DataSource = reader;
+                        GridV.DataBind();
+                    }
+                }
+            }
+        }
+    }
+}
