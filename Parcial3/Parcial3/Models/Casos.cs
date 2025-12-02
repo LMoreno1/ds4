333e6981496e8811d87860256c9eb62c121b54c7
LUIS MORENO <luis.moreno16@utp.ac.pa>
2025-12-01 23:53:29 -0500

    Parcial 3 completo

---
+++ Parcial3/Parcial3/Models/Casos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Parcial3.Models
+{
+    [Table("LM_Casos")]
+    public class Casos
+    {
+        [Key]
+        public int CasoID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NumeroCaso { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Titulo { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Descripcion { get; set; }
+
+        public DateTime FechaInicio { get; set; }
+
+        public DateTime? FechaVencimiento { get; set; }
+
+        [StringLength(50)]
+        public string Estado { get; set; }
+
+        [StringLength(20)]
+        public string Prioridad { get; set; }
+
+        [ForeignKey("Abogado")]
+        public int? AbogadoID { get; set; }
+        public virtual Usuarios Abogado { get; set; }
+
+        [ForeignKey("Cliente")]
+        public int? ClienteID { get; set; }
+        public virtual Clientes Cliente { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+
+        public DateTime? FechaModificacion { get; set; }
+    }
+}
---
+++ Parcial3/Parcial3/Models/Usuarios.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Parcial3.Models
+{
+    [Table("LM_Usuarios")]
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string ContraseñaHash { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string NombreCompleto { get; set; }
+
+        [StringLength(20)]
+        public string Rol { get; set; }
+
+        [StringLength(150)]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        public string Telefono { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaRegistro { get; set; }
+    }
+}
